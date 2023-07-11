e9f84a713e760585d1170fd4ef7b7d25eec62865
Kelvin okumba <okumbakelvin@gmail.com>
2023-07-11 11:13:37 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 
 // Write your code here
-const num1=1;
+const num1=2;
 const num2=31;
+let multiply=num1*num2;
+
 let random =1;
 random ++;
-let multiply=62;
+
 let num3=18;
 let num4=7;
 let mod = num3%num4;
+
 let set = [ 3, 12, 5, 20, 7, 14 ];
 let max = Math.max.apply(null, set);
